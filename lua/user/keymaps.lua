409b5f19c59b64842f5af63e835e7bf47dd91ca9
Hrishi2312 <agrawalhrishikesh23@gmail.com>
2022-02-28 18:55:21 -0500

    Removed Buffer Navigation keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -20,9 +20,6 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
-
 keymap("i", "ii", "<Esc>", opts)
 keymap("i", "II", "<Esc>", opts)
 keymap("v", "ii", "<Esc>", opts)
